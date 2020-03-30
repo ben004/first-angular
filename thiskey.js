a61f0860b567377fe332e547ce258c6c45cf7cdb
ben004 <benbeniel007@gmail.com>
2020-03-30 16:16:25 +0530

    files

---
+++ thiskey.js
@@ -0,0 +1,25 @@
+let speed
+class Animal {
+
+    constructor (speed)
+    {
+        this.speed=10;
+    }
+    
+    stop() {
+      console.log(` stands still.`);
+    }
+  }
+  class Dog extends Animal
+  { 
+    run() {
+        console.log(` runs with speed ${this.speed}.`);
+    }
+  }
+  let animal= new Animal(10)
+  let dog = new Dog();
+  dog.run()
+  dog.stop()
+
+
+  
